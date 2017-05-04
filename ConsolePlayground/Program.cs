3e095b97aa696b3f7b7b7085f9a44381770759f7
dantte <dev.bankai@gmail.com>
2017-05-04 12:15:29 +0100

    ...

--- ConsolePlayground/Program.cs
+++ ConsolePlayground/Program.cs
@@ -9,21 +9,33 @@ namespace ConsolePlayground
     {
         static void Main(string[] args)
         {
-            using (var rng = new RNGCryptoServiceProvider())
+            var op = LazyOp.Try(() =>
             {
-                for (int cnt = 0; cnt < 50; cnt++)
-                {
-                    Console.WriteLine(rng.RandomSignedInt());
-                }
-
-                Console.ReadKey();
-                for (int cnt = 0; cnt < 50; cnt++)
-                {
-                    Console.WriteLine(rng.RandomInt(10, 20));
-                }
-            }
-
+                Console.WriteLine("starting");
+            })
+            .Then(() => SomeOperation(), null)
+            .Then(_v =>
+            {
+                Console.WriteLine($"last one returned: {_v}");
+                return _v;
+            })
+            .Then(_v =>
+            {
+                throw new Exception("oops");
+            })
+            .ContinueWith(_op =>
+            {
+                Console.WriteLine($"thrown exception is {_op.GetException()}");
+                return "new value";
+            });
+            
+            Console.WriteLine($"Before resolving, Result = {op.Result}");
+            op.Resolve();
+            Console.WriteLine($"After resolving, Result = {op.Result}");
+            
             Console.ReadKey();
         }
+
+        public static IOperation<string> SomeOperation() => AsyncOp.Try((() => "yes"));
     }
 }
