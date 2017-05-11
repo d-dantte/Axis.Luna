411f9c2e3310df666a71d1d7165ac6ccc9b206f5
dantte <dev.bankai@gmail.com>
2017-05-11 09:56:22 +0100

    set the author (AssemblyCompany), and description (AssemblyDescription)

--- Axis.Luna/Properties/AssemblyInfo.cs
+++ Axis.Luna/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Axis.Luna")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Common Extension methods and utility classes")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Axiis")]
 [assembly: AssemblyProduct("Axis.Luna")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,6 +33,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 //[assembly: AssemblyVersion("1.0.0.0")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: AssemblyVersion("3.0.3")]
-[assembly: AssemblyFileVersion("3.0.3")]
