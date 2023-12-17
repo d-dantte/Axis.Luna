fe180a43983edfa3e629b586cc4215d5bad12a98
dantte <stanley.damasus@gmail.com>
2023-12-17 11:30:35 +0100

    adds ValueUnion

--- Axis.Luna.Common/Unions/Union2.cs
+++ Axis.Luna.Common/Unions/Union2.cs
@@ -165,4 +165,122 @@ namespace Axis.Luna.Common.Unions
 
         #endregion
     }
+
+    public readonly struct ValueUnion<T1, T2> :
+        IUnion<T1, T2, ValueUnion<T1, T2>>
+    {
+        private readonly object _value;
+
+        object IUnion<T1, T2, ValueUnion<T1, T2>>.Value => _value;
+
+        #region Construction
+
+        // Remove this if a compile-time check for distinct generic types is available
+        static ValueUnion()
+        {
+            var types = new HashSet<Type>
+            {
+                typeof(T1), typeof(T2)
+            };
+
+            if (types.Count != 2)
+                throw new InvalidOperationException("Invalid generic types: duplicate types found");
+        }
+
+        public ValueUnion(object value)
+        {
+            _value = value switch
+            {
+                null => null,
+                T1 or T2 => value,
+                _ => throw new ArgumentOutOfRangeException(
+                    nameof(value),
+                    $"Invalid {nameof(value)} type: '{value.GetType()}'")
+            };
+        }
+        #endregion
+
+        #region Is
+
+        public bool Is(out T1 value) => Is(_value, out value);
+
+        public bool Is(out T2 value) => Is(_value, out value);
+
+        public bool IsNull() => _value is null;
+
+        private static bool Is<T>(object unionValue, out T value)
+        {
+            if (unionValue is T t1)
+            {
+                value = t1;
+                return true;
+            }
+
+            value = default;
+            return false;
+        }
+
+        #endregion
+
+        #region Map
+
+        public TOut MapMatch<TOut>(
+            Func<T1, TOut> t1Mapper,
+            Func<T2, TOut> t2Mapper,
+            Func<TOut> nullMap = null)
+        {
+            ArgumentNullException.ThrowIfNull(t1Mapper);
+            ArgumentNullException.ThrowIfNull(t2Mapper);
+
+            if (_value is T1 t1)
+                return t1Mapper.Invoke(t1);
+
+            if (_value is T2 t2)
+                return t2Mapper.Invoke(t2);
+
+            // unknown type, assume null
+            return nullMap switch
+            {
+                null => default,
+                _ => nullMap.Invoke()
+            };
+        }
+
+        #endregion
+
+        #region Consume
+
+        public void ConsumeMatch(
+            Action<T1> t1Consumer,
+            Action<T2> t2Consumer,
+            Action nullConsumer = null)
+        {
+            ArgumentNullException.ThrowIfNull(t1Consumer);
+            ArgumentNullException.ThrowIfNull(t2Consumer);
+
+            if (_value is T1 t1)
+                t1Consumer.Invoke(t1);
+
+            else if (_value is T2 t2)
+                t2Consumer.Invoke(t2);
+
+            else if (_value is null && nullConsumer is not null)
+                nullConsumer.Invoke();
+        }
+
+        #endregion
+
+        #region With
+
+        public ValueUnion<T1, T2> WithMatch(
+            Action<T1> t1Consumer,
+            Action<T2> t2Consumer,
+            Action nullConsumer = null)
+        {
+            ConsumeMatch(t1Consumer, t2Consumer, nullConsumer);
+            return this;
+        }
+
+        #endregion
+    }
 }
--- Axis.Luna.Common/Unions/Union5.cs
+++ Axis.Luna.Common/Unions/Union5.cs
@@ -228,4 +228,161 @@ namespace Axis.Luna.Common.Unions
 
         #endregion
     }
+
+    public readonly struct ValueUnion<T1, T2, T3, T4, T5> :
+        IUnion<T1, T2, T3, T4, T5, ValueUnion<T1, T2, T3, T4, T5>>
+    {
+        private readonly object _value;
+
+        object IUnion<T1, T2, T3, T4, T5, ValueUnion<T1, T2, T3, T4, T5>>.Value => _value;
+
+        #region Construction
+
+        // Remove this if a compile-time check for distinct generic types is available
+        static ValueUnion()
+        {
+            var types = new HashSet<Type>
+            {
+                typeof(T1), typeof(T2), typeof(T3), typeof(T4), typeof(T5)
+            };
+
+            if (types.Count != 5)
+                throw new InvalidOperationException("Invalid generic types: duplicate types found");
+        }
+
+        public ValueUnion(object value)
+        {
+            _value = value switch
+            {
+                null => null,
+                T1 or T2 or T3 or T4 or T5 => value,
+                _ => throw new ArgumentOutOfRangeException(
+                    nameof(value),
+                    $"Invalid {nameof(value)} type: '{value.GetType()}'")
+            };
+        }
+        #endregion
+
+        #region Is
+
+        public bool Is(out T1 value) => Is(_value, out value);
+
+        public bool Is(out T2 value) => Is(_value, out value);
+
+        public bool Is(out T3 value) => Is(_value, out value);
+
+        public bool Is(out T4 value) => Is(_value, out value);
+
+        public bool Is(out T5 value) => Is(_value, out value);
+
+        public bool IsNull() => _value is null;
+
+        private static bool Is<T>(object unionValue, out T value)
+        {
+            if (unionValue is T t1)
+            {
+                value = t1;
+                return true;
+            }
+
+            value = default;
+            return false;
+        }
+
+        #endregion
+
+        #region Map
+
+        public TOut MapMatch<TOut>(
+            Func<T1, TOut> t1Mapper,
+            Func<T2, TOut> t2Mapper,
+            Func<T3, TOut> t3Mapper,
+            Func<T4, TOut> t4Mapper,
+            Func<T5, TOut> t5Mapper,
+            Func<TOut> nullMap = null)
+        {
+            ArgumentNullException.ThrowIfNull(t1Mapper);
+            ArgumentNullException.ThrowIfNull(t2Mapper);
+            ArgumentNullException.ThrowIfNull(t3Mapper);
+            ArgumentNullException.ThrowIfNull(t4Mapper);
+            ArgumentNullException.ThrowIfNull(t5Mapper);
+
+            if (_value is T1 t1)
+                return t1Mapper.Invoke(t1);
+
+            if (_value is T2 t2)
+                return t2Mapper.Invoke(t2);
+
+            if (_value is T3 t3)
+                return t3Mapper.Invoke(t3);
+
+            if (_value is T4 t4)
+                return t4Mapper.Invoke(t4);
+
+            if (_value is T5 t5)
+                return t5Mapper.Invoke(t5);
+
+            // unknown type, assume null
+            return nullMap switch
+            {
+                null => default,
+                _ => nullMap.Invoke()
+            };
+        }
+
+        #endregion
+
+        #region Consume
+
+        public void ConsumeMatch(
+            Action<T1> t1Consumer,
+            Action<T2> t2Consumer,
+            Action<T3> t3Consumer,
+            Action<T4> t4Consumer,
+            Action<T5> t5Consumer,
+            Action nullConsumer = null)
+        {
+            ArgumentNullException.ThrowIfNull(t1Consumer);
+            ArgumentNullException.ThrowIfNull(t2Consumer);
+            ArgumentNullException.ThrowIfNull(t3Consumer);
+            ArgumentNullException.ThrowIfNull(t4Consumer);
+            ArgumentNullException.ThrowIfNull(t5Consumer);
+
+            if (_value is T1 t1)
+                t1Consumer.Invoke(t1);
+
+            else if (_value is T2 t2)
+                t2Consumer.Invoke(t2);
+
+            else if (_value is T3 t3)
+                t3Consumer.Invoke(t3);
+
+            else if (_value is T4 t4)
+                t4Consumer.Invoke(t4);
+
+            else if (_value is T5 t5)
+                t5Consumer.Invoke(t5);
+
+            else if (_value is null && nullConsumer is not null)
+                nullConsumer.Invoke();
+        }
+
+        #endregion
+
+        #region With
+
+        public ValueUnion<T1, T2, T3, T4, T5> WithMatch(
+            Action<T1> t1Consumer,
+            Action<T2> t2Consumer,
+            Action<T3> t3Consumer,
+            Action<T4> t4Consumer,
+            Action<T5> t5Consumer,
+            Action nullConsumer = null)
+        {
+            ConsumeMatch(t1Consumer, t2Consumer, t3Consumer, t4Consumer, t5Consumer, nullConsumer);
+            return this;
+        }
+
+        #endregion
+    }
 }
